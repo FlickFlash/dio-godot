c3a9f6bc927479f331021bd2d385e18d5b1e585d
FlickFlash <joseroberto_fsa@live.com>
2024-07-07 12:59:21 -0300

    Nova solução que não soluciona cor da label

--- Tiny Swords/misc/damage_number.gd
+++ Tiny Swords/misc/damage_number.gd
@@ -7,17 +7,19 @@ var damage_type: String = ""
 
 func _ready() -> void:
 	#var number = Label.new()
-	Damage_Label.label_settings = LabelSettings.new()
-	Damage_Label.label_settings.font_size = 24
+	#Damage_Label.label_settings = LabelSettings.new()
+	#Damage_Label.label_settings.font_size = 24
 	#Damage_Label.label_settings.font = SystemFont <<<<<<<< font_names ???
 	
 	Damage_Label.text = str(value)
 	match damage_type:
 		"physical_type":
-			Damage_Label.label_settings.font_color = Color(1, 0.353, 0.361)
+			#Damage_Label.label_settings.font_color = Color(1, 0.353, 0.361)
+			Damage_Label.add_theme_color_override("font_color", Color(1, 0.353, 0.361))
 			print(damage_type)
 		"ritual_type":
-			Damage_Label.label_settings.font_color = Color(0.306, 0.353, 0.773)
+			#Damage_Label.label_settings.font_color = Color(0.306, 0.353, 0.773)
+			Damage_Label.add_theme_color_override("font_color", Color(0.306, 0.353, 0.773))
 			print(damage_type)
 
 #func _process(_delta: float) -> void:
